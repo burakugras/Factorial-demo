3c26e496a560c3632470bf899d0b96d19122b86e
burakugras <burakugras67@gmail.com>
2022-02-11 18:23:48 +0300

    created project

---
+++ dnmFactorial/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace dnmFactorial
+{
+    class FactorialTest
+    {
+        static void Main()
+        {
+            for (long counter = 0; counter <=10;++counter)
+            {
+                Console.WriteLine($"{counter}! = {Factorial(counter)}");
+            }
+        }
+
+        static long Factorial(long number)
+        {
+            if(number<=1)
+            {
+                return 1;
+            }
+            else
+            {
+                return number * Factorial(number - 1);
+            }
+        }
+        
+    }
+}
